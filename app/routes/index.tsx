776b3cc4e9d5ff822adfcd1b2c689bf0f533d05b
homumado <nobisan@gmail.com>
2023-01-18 11:27:21 -1000

    this works for some reason.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,37 @@
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
+import { defer } from "@remix-run/node";
+
+function getCritical(): Promise<{ message: string }> {
+  return new Promise((r) =>
+    setTimeout(() => r({ message: "Immediate Data" }), 1000)
+  );
+}
+function getLazy(): Promise<{ message: string }> {
+  return new Promise((r) =>
+    setTimeout(() => r({ message: "Slow Data" }), 4000)
+  );
+}
+
+export const loader = async () => {
+  const critical = await getCritical();
+  const lazy = getLazy();
+  return defer({
+    critical,
+    lazy,
+  });
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <section>Critical Data: {JSON.stringify(data.critical)}</section>
+      <Suspense fallback={<section>Lazy Data: Loading....</section>}>
+        <Await resolve={data.lazy} errorElement={<div>Loading...</div>}>
+          {(value) => <section>Lazy Data: {JSON.stringify(value)}</section>}
+        </Await>
+      </Suspense>
     </div>
   );
 }
